8005110e7d4d4e009550f81c6066466f97b7a3b6
danielrugutt <danielrugutt2002@gmail.com>
2018-12-30 03:04:18 -0600

    Created variables to store required info for the password generator

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,6 +1,10 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,15 +17,29 @@ public class PasswordGenerator extends JFrame {
     JLabel jl = new JLabel();
     JTextField jt = new JTextField("Type important numbers, then press \"Enter\" ", 25);
     JButton jb = new JButton("Done");
-    public static boolean isDone = false;
-    public static int labelCount = 0;
+    JCheckBox symbol, lower, upper, ambiguous;
+
+	public static boolean isDone = false;
+    public static String numString = "";
+
+    public static boolean isSymbol = false;
+    public static boolean isLower = false;
+    public static boolean isUpper = false;
+    public static boolean isAmbiguous = false;
 
     public PasswordGenerator() {
         setTitle("Password Generator");
         setVisible(true);
-        setSize(375, 300);
+        setSize(450, 300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        symbol = new JCheckBox("Any Symbols? (e.g. @#$% )");
+        lower = new JCheckBox("Any Lowercase Letters?");
+        upper = new JCheckBox("Any Uppercase Letters?");
+        ambiguous = new JCheckBox("Any Ambiguous Characters? (e.g. { } [ ] ( ) / \\ ' \"\" ` ~ , ; : . < > )");
+
         jp.add(jt);
+
         jt.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String input = jt.getText();
@@ -31,21 +49,72 @@ public class PasswordGenerator extends JFrame {
                         char c = input.charAt(i);
                         if (c < '0' || c > '9') {
                             jl.setText("Please input a valid number");
+                            break;
                         }
                     }
+                    numString += input + " ";
                 } else {
                     jl.setText("Please input a valid number");
                 }
 
             }
         });
-        jp.add(jb);
+
+        jp.add(jl);
+
+        symbol.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if (symbol.isSelected()) {
+                    isSymbol = true;
+                } else {
+                    isSymbol = false;
+                }
+            }
+        });
+
+        lower.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if (lower.isSelected()) {
+                    isLower = true;
+                } else {
+                    isLower = false;
+                }
+            }
+        });
+
+        upper.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if (upper.isSelected()) {
+                    isUpper = true;
+                } else {
+                    isUpper = false;
+                }
+            }
+        });
+
+        ambiguous.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if (ambiguous.isSelected()) {
+                    isAmbiguous = true;
+                } else {
+                    isAmbiguous = false;
+                }
+            }
+        });
+
+        jp.add(symbol);
+        jp.add(lower);
+        jp.add(upper);
+        jp.add(ambiguous);
+
         jb.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 isDone = true;
             }
         });
-        jp.add(jl);
+
+        jp.add(jb);
+
         add(jp);
     }
 
@@ -54,9 +123,9 @@ public class PasswordGenerator extends JFrame {
         while (isDone == false) {
             try {
                 Thread.sleep(200);
-            } catch (InterruptedException i) {
-            }
+            } catch (InterruptedException i) {}
         }
+        System.out.println(numString);
     }
 
     public static String passwordGenerator() {
